e591a5b1f2994487e9c881aee2b3b5bb5fcadeca
zeFresk <8807862+zeFresk@users.noreply.github.com>
2022-04-19 15:39:23 +0200

    connection ftp

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
+import org.apache.commons.net.ftp.*;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Download ftp references files
+        // https://commons.apache.org/proper/commons-net/apidocs/org/apache/commons/net/ftp/FTPClient.html
+        FTPClient ftp = new FTPClient();
+        FTPClientConfig config = new FTPClientConfig();
+        config.setServerTimeZoneId("Europe/Paris");
+        ftp.configure(config);
+
+        boolean error = false;
+        try {
+            String server = "ftp.ncbi.nlm.nih.gov";
+            ftp.connect(server);
+            System.out.println("Connected to: " + server);
+            System.out.print(ftp.getReplyString());
+        } catch (IOException e) {
+            error = true;
+            e.printStackTrace();
+        } finally {
+            if (ftp.isConnected()) {
+                try {
+                    ftp.disconnect();
+                } catch (IOException ioe) {
+                    // nothing
+                }
+            }
+        }
+        System.exit(error ? 1 : 0);
     }
 }
