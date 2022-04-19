ca7004265883c6c132980ecad861d1b123366488
zeFresk <8807862+zeFresk@users.noreply.github.com>
2022-04-19 18:54:16 +0200

    Refactoring

---
+++ src/main/java/org/example/Ftp.java
@@ -0,0 +1,122 @@
+package org.example;
+
+import org.apache.commons.net.ftp.*;
+
+import java.io.*;
+import java.util.*;
+
+public class Ftp {
+    private FTPClient ftpClient;
+    private HashMap<String, FTPFile> fileHashMap;
+    private String server;
+    private boolean logged;
+
+    private void connect() throws IOException {
+        try {
+            ftpClient.connect(server);
+            //System.out.println("Connected to: " + server);
+            //System.out.print(ftpClient.getReplyString());
+
+            int reply = ftpClient.getReplyCode();
+
+            if (!FTPReply.isPositiveCompletion(reply)) {
+                ftpClient.disconnect();
+                throw new IOException("FTP server refused connection.");
+            }
+
+        } catch (Throwable e) {
+            throw new IOException("Unable to connnect to ftp server: " + e.getMessage(), e);
+        }
+    }
+
+    private void login() throws IOException {
+        logged = ftpClient.login("anonymous", "");
+        if (!logged)
+            throw new IOException("Login failed");
+    }
+
+    public boolean isConnected() {
+        var co = ftpClient.isConnected();
+        if (co)
+            logged = false;
+        return co;
+    }
+
+    public boolean isLogged() {
+        return logged;
+    }
+
+    private void prepare() throws IOException {
+        if (!isConnected())
+            connect();
+        if (!isLogged())
+            login();
+    }
+
+    public String temp_directory() {
+        return Config.data_directory() + "/" + server.replace(".", "_");
+    }
+
+    public Ftp(String server) throws IOException, RuntimeException {
+        this.server = server;
+        this.fileHashMap = new HashMap<String, FTPFile>();
+        // Download ftp references files
+        // https://commons.apache.org/proper/commons-net/apidocs/org/apache/commons/net/ftp/FTPClient.html
+        ftpClient = new FTPClient();
+        ftpClient.setControlKeepAliveTimeout(300);
+        //ftp.setControlEncoding("UTF-8");
+        //ftp.setAutodetectUTF8(true);
+        FTPClientConfig config = new FTPClientConfig();
+        //config.setServerTimeZoneId("Europe/Paris");
+        ftpClient.configure(config);
+
+        var dir = new File(temp_directory());
+        if (!dir.exists() || !dir.isDirectory())
+            if (!dir.mkdirs())
+                throw new RuntimeException("Can't create temporary directory");
+    }
+
+    public void close() throws IOException {
+        if (isLogged())
+            ftpClient.logout();
+        if (isConnected())
+            ftpClient.disconnect();
+    }
+
+    public File getFile(String ftp_path) throws IOException {
+        prepare();
+
+        try {
+            File localFile = new File(temp_directory() + "/" + ftp_path);
+            File parentDirectory = localFile.getParentFile();
+            if (!parentDirectory.exists() || !parentDirectory.isDirectory())
+                if (!parentDirectory.mkdirs())
+                    throw new IOException("Cannot create directories for " + ftp_path);
+
+            if (!fileHashMap.containsKey(ftp_path)) {
+                FTPFile ftpFile = ftpClient.mlistFile(ftp_path);
+                if (ftpFile == null)
+                    throw new RuntimeException(ftp_path + " does not exist");
+                fileHashMap.put(ftp_path, ftpFile);
+            }
+
+            if ((!localFile.exists() && !localFile.isDirectory()) || (localFile.lastModified() < fileHashMap.get(ftp_path).getTimestamp().getTimeInMillis())) { // remote is newer
+                var outs = new FileOutputStream(localFile);
+                ftpClient.retrieveFile(ftp_path, outs);
+            }
+
+            return localFile;
+        } catch (Throwable t) {
+            throw new IOException("Unable to download " + ftp_path, t);
+        }
+    }
+
+    public void cacheMetadata(String path) throws IOException {
+        prepare();
+
+        var files = ftpClient.listFiles(path);
+        for (var f : files) {
+            fileHashMap.put(f.getName(), f);
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,65 +1,19 @@
 package org.example;
-import org.apache.commons.net.ftp.*;
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        // Download ftp references files
-        // https://commons.apache.org/proper/commons-net/apidocs/org/apache/commons/net/ftp/FTPClient.html
-        FTPClient ftp = new FTPClient();
-        //ftp.setControlEncoding("UTF-8");
-        //ftp.setAutodetectUTF8(true);
-        FTPClientConfig config = new FTPClientConfig();
-        //config.setServerTimeZoneId("Europe/Paris");
-        ftp.configure(config);
-
+    public static void main(String[] args) throws IOException {
+        Ftp ftp = null;
         boolean error = false;
         try {
-            String server = "ftp.ncbi.nlm.nih.gov";
-            ftp.connect(server);
-            System.out.println("Connected to: " + server);
-            System.out.print(ftp.getReplyString());
+            ftp = new Ftp("ftp.ncbi.nlm.nih.gov");
 
-            int reply = ftp.getReplyCode();
-
-            if(!FTPReply.isPositiveCompletion(reply)) {
-                ftp.disconnect();
-                System.err.println("FTP server refused connection.");
-                System.exit(1);
-            }
-
-            System.out.println("Login...");
-            boolean success = ftp.login("anonymous", "");
-            if (!success)
-                throw new IOException("Login failed");
-
-            System.out.println("Retrieving files...");
-            if (!ftp.changeWorkingDirectory("/genomes/GENOME_REPORTS/"))
-                throw new IOException("Directory not found");
-            // file list
-            var files = ftp.listFiles();
-            Map<String, FTPFile> fileMap = new HashMap<String, FTPFile>();
-            for (var f : files) {
-                fileMap.put(f.getName(), f);
-                System.out.println(f.getName());
-            }
-
-            // Index
-            String wd = System.getProperty("user.dir") + "/data";
-            var datadir = new File(wd);
-            if (! datadir.exists() || ! datadir.isDirectory())
-                datadir.mkdir();
-
-            var index = new File(wd + "/overview.txt");
-            if ((!index.exists() && !index.isDirectory()) || (index.lastModified() < fileMap.get("overview.txt").getTimestamp().getTimeInMillis()) ){
-                var outs = new FileOutputStream(index);
-                ftp.retrieveFile("overview.txt", outs);
-            }
+            File overview = ftp.getFile("/genomes/GENOME_REPORTS/overview.txt");
 
             // parsing overview
-            var br = new BufferedReader(new FileReader(index));
+            var br = new BufferedReader(new FileReader(overview));
             Vector<String[]> overview_data = new Vector<String[]>();
             String line;
             while ((line = br.readLine()) != null) {
@@ -74,20 +28,13 @@ public class Main {
                 }
                 System.out.print("\n");
             }
-
-            ftp.logout();
-        } catch (IOException e) {
+        } catch (Exception e) {
             error = true;
             e.printStackTrace();
         } finally {
-            if (ftp.isConnected()) {
-                try {
-                    ftp.disconnect();
-                } catch (IOException ioe) {
-                    // nothing
-                }
-            }
+            if (ftp != null)
+                ftp.close();
+            System.exit(error ? 1 : 0);
         }
-        System.exit(error ? 1 : 0);
     }
 }
