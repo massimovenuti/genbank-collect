c262641d623c00e6c3a786805aa8823e0e4da39f
zeFresk <8807862+zeFresk@users.noreply.github.com>
2022-05-03 15:06:50 +0200

    bdd ok ?

--- src/main/java/org/NcbiParser/Bdd.java
+++ src/main/java/org/NcbiParser/Bdd.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
     - Organism : Kingdom, Group, Subgroup, nom
     - Date : Year, Month, Day, timestamp
     - Overview : Organism, Date (celle dans overview)
-    - NC : Organism, NC (String)
+    [- NC : Organism, NC (String)] pas nécessaire ?
     - SeqType : nom (CDS, Codon, etc.)
     - ParsingResult : Organism, Date (celle des fichiers locaux, ou bien la même que celle dans overview à ce moment là), nb_files_parsed (int)
     [[- Files : (NC ou Organism ?), Date de création)] pas franchement nécessaire]
@@ -21,7 +21,7 @@ public class Bdd {
         Met à jour la base avec les dernières données d'overview, viruses.txt, x.ids, etc.
      */
     public static void updateFromOverview(ArrayList<OverviewData> overview_parsed) { }
-    public static void updateFromIds(ArrayList<IdsData> ids_parsed) { }
+    //public static void updateFromIds(ArrayList<IdsData> ids_parsed) { } // pas nécessaire ?
     public static void updateFromIndexFilre(ArrayList<IndexData> index_parsed) { }
 
     /*
@@ -29,7 +29,7 @@ public class Bdd {
         lesquels les deux dates (celle dans overview, et la dernière maj locale) diffèrent
         normalement juste une comparaison de deux colonnes, SQL fait tout le taf
      */
-    public static ArrayList<int> all_ids_needing_update() { }
+    public static ArrayList<Integer> all_ids_needing_update() { }
 
     /*
         Récupère la hiérarchie depuis la BDD
