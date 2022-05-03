b4a465afd5165a2c879d57b90c03ccd8a752fe4d
neiko <neiko.damoi@etu.unistra.fr>
2022-05-03 17:08:36 +0200

    main error

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,25 +5,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Ncbi ncbi = null;
-        boolean error = false;
-        try {
-            ncbi = new Ncbi();
-            var rd = ncbi.createRawMetaData("eukaryotes");
-            for (var meta : rd) {
-                System.out.printf("%20s %20s %20s\n", meta.getGroup(), meta.getSubgroup(), meta.getOrganism());
-            }
 
-            var hier = ncbi.getHierarchy();
-
-
-            System.out.println(hier);
-
-        } catch (Exception e) {
-            error = true;
-            e.printStackTrace();
-        } finally {
-            System.exit(error ? 1 : 0);
-        }
     }
 }
