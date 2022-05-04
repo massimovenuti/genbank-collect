81b1c2dab7fa3403a0f9c006bb32683810b209e9
Massimo VENUTI <massimo.venuti@etu.unistra.fr>
2022-05-04 15:26:53 +0200

    Implemented parsing task

--- src/main/java/org/NcbiParser/ParsingTask.java
+++ src/main/java/org/NcbiParser/ParsingTask.java
@@ -1,15 +1,30 @@
 package org.NcbiParser;
 
+import org.biojava.nbio.core.exceptions.CompoundNotFoundException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public abstract class ParsingTask implements Task {
-    public ParsingTask(String gbffPath, String outDir) {
+    String gbffPath, outDir, organism, organelle;
+    String[] regions;
 
-    }
-    public ParsingTask(String fastaPath, String gbPath, String outDir) {
-
+    public ParsingTask(String gbffPath, String outDir, String organism, String organelle, String[] regions) {
+        this.gbffPath= gbffPath;
+        this.outDir= outDir;
+        this.organism = organism;
+        this.organelle = organelle;
+        this.regions = regions;
     }
 
     @Override
     public boolean run(MultiTasker mt) {
+        try {
+            GbffParser parser = new GbffParser(gbffPath);
+            return parser.parse_into(outDir, organism, organelle, regions);
+        } catch (IOException | CompoundNotFoundException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 }
