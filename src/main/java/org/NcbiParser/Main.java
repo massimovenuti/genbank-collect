8af6555fa4e2bf8da36cdd0cb3d90d95a00ee6e8
Massimo VENUTI <massimo.venuti@etu.unistra.fr>
2022-05-04 11:27:17 +0200

    Implementing parser with new design

--- src/main/java/org/NcbiParser/Main.java
+++ src/main/java/org/NcbiParser/Main.java
@@ -1,7 +1,27 @@
 package org.NcbiParser;
 
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String gbffPath = "data/sequence_10.gb";
+        String outDir = "Results/Eukaryota/Animals/Amphibians/";
+        String organism = "Homo Sapiens";
+        String[] regions = {"CDS"};
+
+        try {
+            GbffParser parser = new GbffParser(gbffPath);
+
+            parser.setOrganism(organism);
+            parser.setRegions(regions);
+
+            parser.parse_into(outDir);
+
+        } catch (FileNotFoundException e) {
+            System.err.println("[ERROR] Failed to open file");
+        } catch (Exception e) {
+            System.err.println("[ERROR] Error occurred while parsing");
+        }
     }
 }
