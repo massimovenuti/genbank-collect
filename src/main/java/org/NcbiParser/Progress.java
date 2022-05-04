8256897546f5e82665805d8502bdc9dc4ceb56d7
CHENEAU LEO <leo.cheneau@etu.unistra.fr>
2022-05-04 13:52:30 +0000

    Progress et ProgressTask

--- src/main/java/org/NcbiParser/Progress.java
+++ src/main/java/org/NcbiParser/Progress.java
@@ -7,7 +7,18 @@ Répertorie les progrès des taches
  */
 public class Progress {
     private ArrayList<ProgressTask> progressTasks;
-    public ProgressTask registerTask(String name) {return null;}
-    public ArrayList<ProgressTask> all_tasks() {return null;}
-    public void remove_task(ProgressTask t) {}
+
+    public ProgressTask registerTask(String name) {
+        progressTasks = new ArrayList<ProgressTask>();
+        progressTasks.add(new ProgressTask(name));
+        return progressTasks.get(progressTasks.size() - 1);
+    }
+
+    public ArrayList<ProgressTask> all_tasks() {
+        return progressTasks;
+    }
+
+    public void remove_task(ProgressTask t) {
+        progressTasks.remove(t);
+    }
 }
