412db9e72680bdc9b0d0d1cf8a01d12c2dc569b8
Massimo VENUTI <massimo.venuti@etu.unistra.fr>
2022-05-05 11:32:47 +0200

    tree setter for gui

--- src/main/java/org/NcbiParser/GlobalGUIVariables.java
+++ src/main/java/org/NcbiParser/GlobalGUIVariables.java
@@ -1,11 +1,22 @@
 package org.NcbiParser;
 
+import com.sun.source.tree.Tree;
+
 public class GlobalGUIVariables {
     private static GUIVariables guiVariables;
+    private static TreeNode tree;
 
     public static GUIVariables get() {
         if (guiVariables == null)
             guiVariables = new GUIVariables();
         return guiVariables;
+    }
+
+    public static void setTree(TreeNode node) {
+        tree = node;
+    }
+
+    public static TreeNode getTree() {
+        return tree;
     }
 }
